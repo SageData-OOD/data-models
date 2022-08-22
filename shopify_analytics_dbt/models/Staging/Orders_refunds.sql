2900a6e6227096be2bee1ed03bb882c8c6d5e894
bammeked <bammeked@coventry.ac.uk>
2022-08-22 16:52:13 +0100

    Source, tests and documentation added

--- shopify_analytics_dbt/models/Staging/Orders_refunds.sql
+++ shopify_analytics_dbt/models/Staging/Orders_refunds.sql
@@ -1,6 +1,6 @@
 select
     orders.*, 
     coalesce(refund_amount.amount::real, 0) refund_amount
-from dev.public.sd__redshift__xl_replicate__sd__shopify__xl__orders as orders
-left join dev.public.sd__redshift__xl_replicate__sd__shopify__xl__orders__refunds__order_adjustments as refund_amount
+from {{source('shopify_raw_data', 'sd__redshift__xl_replicate__sd__shopify__xl__orders')}} as orders
+left join {{source('shopify_raw_data', 'sd__redshift__xl_replicate__sd__shopify__xl__orders__refunds__order_adjustments')}} as refund_amount
 on orders.id=refund_amount.id
