a283ed64d0c58b6724ce041c182f95f812782820
bammeked <bammeked@coventry.ac.uk>
2022-09-04 15:47:29 +0100

    final rfm table

--- shopify_analytics_dbt/models/Report/rfm_segmentation.sql
+++ shopify_analytics_dbt/models/Report/rfm_segmentation.sql
@@ -17,10 +17,23 @@ case when rfm_cell in ('355','255') then 'Cannot lose'
      when rfm_cell in ('335','334','325','324') then 'Potential churners'
 else 'Other' end as rfm_segment
 from (
-SELECT customer__id,
+select customer__id,
 	ntile(5) over (order by recent_order) as rfm_recency,
 	ntile(5) over (order by total_orders) as rfm_frequency,
 	ntile(5) over (order by total_sales) as rfm_monetary
-from {{ref('rfm_overview')}}
+from (
+select
+    --o.email,
+    o.customer__id,
+    max(o.created_at) recent_order,
+    count(o.id) total_orders,
+    sum(o.total_line_items_price) gross_sales,
+    sum(o.total_line_items_price)
+    + sum(-coalesce(o.total_discounts::real, 0))
+    + sum(coalesce(o.refund_amount, 0))
+    + (sum(coalesce(o.total_tax::real, 0)) - sum(coalesce(o.tax_returned,0))) 
+    + sum(coalesce(o.shipping_charge,0)) as total_sales
+from {{ref('Orders_refunds')}} as o
+group by 1)
 )
 order by 6 desc
