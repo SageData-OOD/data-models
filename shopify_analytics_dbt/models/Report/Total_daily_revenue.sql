0d34c45b1a3c589692730454207b63b84b6bdf60
bammeked <bammeked@coventry.ac.uk>
2022-09-08 17:36:55 +0100

    daily revenue model revised

--- shopify_analytics_dbt/models/Report/Total_daily_revenue.sql
+++ shopify_analytics_dbt/models/Report/Total_daily_revenue.sql
@@ -6,12 +6,16 @@
 
 select 
     date(created_at::timestamp) order_date,
-    round(sum(subtotal_price::real),2) revenue_before_refund,
-    count(customer__orders_count) number_of_orders,
-    sum(customer__orders_count) number_of_item_sold,
-    sum(refund_amount) as daily_refund_amount,
-    sum(total_discounts) as discount,
-    revenue_before_refund + daily_refund_amount revenue_after_refund 
+    round(sum(total_line_items_price::real),2) daily_gross_sales,
+    count(customer__orders_count) daily_number_of_orders,
+    sum(customer__orders_count) daily_number_of_item_sold,
+    sum(total_discounts) daily_discount,
+    sum(refund_amount) daily_refund_amount,
+    daily_gross_sales - daily_refund_amount daily_net_sales,
+    sum(total_tax) daily_total_taxes,
+    sum(tax_returned) daily_total_tax_returns,
+    sum(shipping_charge) daily_shipping_charge,
+    daily_gross_sales - daily_discount - daily_refund_amount + daily_total_taxes + daily_shipping_charge total_sales 
 from {{ref('Orders_refunds')}}
 group by 
 order_date
